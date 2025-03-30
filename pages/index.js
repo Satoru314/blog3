c0a043efb878ac3df414470d43ef0cc3d94ec81e
satoru314 <2474393t@gsuite.kobe-u.ac.jp>
2025-03-30 22:53:06 +0900

    追加

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
-import Header from '@/components/header.js'
-import Footer from '@/components/footer.js'
+import Layout from '@/components/layout'
 import Hero from '@/components/hero.js'
 import EachPost from '@/components/eachpost.js'
 export default function Home() {
   return(
-  <>
-  <main>
-  <Header/>
+<Layout> 
   <EachPost title="スーパーAI" url="https://aistudio.google.com/"/>
   <EachPost title="youtube" url="https://www.youtube.com"></EachPost>
   <Hero/>
-  </main>
- <Footer/>
- </>
- )
-  
+</Layout>
+  ) 
 }
