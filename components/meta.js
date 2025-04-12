382401a4dc6f94e7bd1a889091837428df469636
satoru314 <2474393t@gsuite.kobe-u.ac.jp>
2025-04-13 02:36:38 +0900

    ６章おわり

--- components/meta.js
+++ components/meta.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import { siteMeta } from '@/lib/constants'
 import { useRouter } from 'next/router'
-const { siteTitle, siteDesc, siteUrl, siteLang, siteLocale, siteType, siteIcon } = siteMeta
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
 
 export default function Meta({ pagetitle, pageDesc }) {
     const title = pagetitle ? `${pagetitle} | ${siteTitle}` : siteTitle
@@ -18,8 +17,15 @@ export default function Meta({ pagetitle, pageDesc }) {
             <meta name="description" content={desc}/>
             <meta property="og:description" content={desc}/>
 
-            <Link rel="canonical" href={url} />
+            <link rel="canonical" href={url} />
             <meta property="og:url" content={url}/>
+
+            <meta property="og:site_name" content={siteTitle}/>
+            <meta property="og:type" content={siteType}/>
+            <meta property="og:locale" content={siteLocale}/>
+
+            <link rel="icon" href={siteIcon}/>
+            <link rel="apple-touch-icon" href={siteIcon}/>
 
         </Head>
 
